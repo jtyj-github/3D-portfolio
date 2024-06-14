dc8195cea62d3be7dafae23b8977a93683945091
Justin <tanyjjustin@gmail.com>
2024-06-14 15:59:43 +0800

    :sparkle: feat: add loader component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
 
 const Home = () => {
     return (
@@ -7,7 +8,14 @@ const Home = () => {
             {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
                 POP-UP Card component
             </div> */}
+            <Canvas 
+                className='w-full h-screen bg-transparent' 
+                camera={{ near: 0.1, far:1000 }}
+            >
+                <Suspense fallback={<Loader />}>
 
+                </Suspense>
+            </Canvas>
         </section>
     )
 }
