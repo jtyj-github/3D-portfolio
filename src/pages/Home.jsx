ecdd798942c6cb597139aaa6904d27731bc2131e
Justin <tanyjjustin@gmail.com>
2024-06-15 17:01:28 +0800

    :zap: feat: create models folder for 3D components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
                 camera={{ near: 0.1, far:1000 }}
             >
                 <Suspense fallback={<Loader />}>
-
+                    
                 </Suspense>
             </Canvas>
         </section>
