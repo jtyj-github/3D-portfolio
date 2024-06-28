a15b41fe9c01f525ec93f33604cd2709aebd416c
Justin <tanyjjustin@gmail.com>
2024-06-28 17:19:10 +0800

    :zap: add sky animation

--- src/models/sky.jsx
+++ src/models/sky.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 
 import skyScene from '../assets/3d/sky.glb'
 
-const sky = () => {
-    const sky = useGLTF(skyScene)
+const sky = ({ isRotating }) => {
+    const sky = useGLTF(skyScene);
+    const skyRef = useRef();
+
+    useFrame((_, deltaX) => {
+        if(isRotating) {
+            skyRef.current.rotation.y += 0.25 * deltaX;
+        }
+    })
+
     return (
-        <mesh>
+        <mesh ref={skyRef}>
             <primitive object={sky.scene} />
         </mesh>
     )
