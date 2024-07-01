a043a6ea004e3aa7fa691b26f87fe45c77e60ad2
Justin <tanyjjustin@gmail.com>
2024-07-02 01:21:32 +0800

    :bug: temp fix: workaround console error when threejs is mounted

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+
+const originalWarn = console.warn;
+    console.warn = message => {
+        if (message.includes("Cannot call the manual advancement of rafz whilst frameLoop is not set as demand")) {
+            return;
+        }
+        originalWarn(message);
+    };
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
